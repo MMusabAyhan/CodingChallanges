d5b1ade9ec920ddf4569e127ca26847c56b0272e
Mustafa Musab Ayhan <mmusabayhan@gmail.com>
2023-08-08 04:23:58 +0300

    Anagram algorithm is improved.

--- Anagrams/Anagrams.cpp
+++ Anagrams/Anagrams.cpp
@@ -1,51 +1,72 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
-bool isAnagram(string& word1, string& word2)
+bool isAnagram(const string& word1, const string& word2)
 {
 	if (word1.size() != word2.size())
-	{
 		return false;
-	}
+
+	unordered_map<char, int> possessed;
+	unordered_map<char, int> seen;
 
 	for (int i = 0; i < word1.size(); i++)
+		possessed[word1[i]]++;
+
+	for (int i = 0; i < word2.size(); i++)
 	{
-		bool matched{ false };
-		char currentLetter = word1[i];
+		seen[word2[i]]++;
 
-		for (int j = 0; j < word1.size(); j++)
-		{
-			if (currentLetter == word2[j])
-			{
-				matched = true;
-				break;
-			}
-		}
-
-		if (!matched)
+		if (seen[word2[i]] > possessed[word2[i]])
 			return false;
 	}
 
 	return true;
 }
 
-
-void updateTemp(vector<string>& temp,const string& str) 
+void findAnagrams(string inputs[], int inputsSize, vector<vector<string>>& outAnagramList) 
 {
-	auto it = temp.begin();
+	unordered_set<string> cachedWords;
 
-	for (;it != temp.end();)
+	for (int i = 0; i < inputsSize - 1; i++)
 	{
-		if (*it == str) 
+		vector<string> currentAnagrams;
+		currentAnagrams.push_back(inputs[i]);
+
+		for (int j = i + 1; j < inputsSize; j++)
 		{
-			it = temp.erase(it);
-			continue;
+			if (cachedWords.find(inputs[j]) == cachedWords.end() && isAnagram(inputs[i], inputs[j]))
+			{
+				currentAnagrams.push_back(inputs[j]);
+				cachedWords.emplace(inputs[j]);
+			}
 		}
 
-		it++;
+		if (currentAnagrams.size() > 1)
+			outAnagramList.push_back(currentAnagrams);
+	}
+}
+
+void printAnagrams(vector<vector<string>>& anagramList)
+{
+	for (int i = 0; i < anagramList.size(); i++)
+	{
+		const vector<string>& currentList = anagramList[i];
+		string output{};
+
+		for (int j = 0; j < currentList.size(); j++)
+		{
+			output += currentList[j];
+			output += '/';
+		}
+
+		output.pop_back();
+
+		cout << output << endl;
 	}
 }
 
@@ -67,54 +88,9 @@ int main()
 		{"TAMHE"},
 	};
 
-	vector<string> temp;
-
-	for (int i = 0; i < inputsize; i++)
-	{
-		temp.push_back(inputs[i]);
-	}
-
 	vector<vector<string>> anagramList;
 
-	while (temp.size() > 1) 
-	{
-		vector<string> currentAnagram;
-		currentAnagram.push_back(temp[0]);
+	findAnagrams(inputs, inputsize, anagramList);
 
-		for (int i = 1; i < temp.size(); i++)
-		{
-			if (isAnagram(temp[0], temp[i]))
-			{
-				currentAnagram.push_back(temp[i]);
-			}
-		}
-
-		if (currentAnagram.size() > 1)
-		{
-			anagramList.push_back(currentAnagram);
-		}
-
-		for (int i = 0; i < currentAnagram.size(); i++)
-		{
-			updateTemp(temp, currentAnagram[i]);
-		}
-	}
-	
-	for (int i = 0; i < anagramList.size(); i++)
-	{
-		
-		const vector<string>& currentList = anagramList[i];
-		string output{};
-
-		for (int j = 0; j < currentList.size(); j++)
-		{
-			output += currentList[j];
-			output += '/';
-		}
-
-		output.pop_back();
-
-		cout << output << endl;
-	}
-
+	printAnagrams(anagramList);
 }
