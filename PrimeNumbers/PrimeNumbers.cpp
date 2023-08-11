8431899694dcbf73607923710b697aad5277eda8
mmusabayhan <mmusabayhan@gmail.com>
2023-08-11 14:34:42 +0300

    Prime number algorithm changed

--- PrimeNumbers/PrimeNumbers.cpp
+++ PrimeNumbers/PrimeNumbers.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <vector>
 #include <math.h>
-
-using std::vector;
 
 bool isPrime(const int& number)
 {
+	if (number < 2)
+		return false;
+
 	const int lastNumberToCheck = sqrt(number);
 
 	for (int i = 2; i <= lastNumberToCheck; i++)
@@ -23,8 +23,6 @@ int SumPrimes(int first, int last)
 
 	for (int i = first; i <= last; i++)
 	{
-		if (i == 1) { continue; }
-
 		if (isPrime(i))
 			sum += i;
 	}
@@ -34,6 +32,6 @@ int SumPrimes(int first, int last)
 
 int main()
 {
-	std::cout << SumPrimes(5, 17);
+	std::cout << SumPrimes(1, 5);
 }
 
